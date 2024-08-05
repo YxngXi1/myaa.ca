bb08ecab03fb547240c4ddad38d4b427deefe7b8
YxngXi1 <xuetianyang@gmail.com>
2024-08-04 20:40:36 -0400

    finished events page + added loading animations for lazy loading

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import Event from '@/components/Event';
 
 export default function Home() {
   return (
-    <main>
-      <div className="absolute z-0 bg-black min-h-screen w-full flex justify-center items-center text-center flex-col fadeOut">
+    <main className="">
+      <div className="fixed z-0  min-h-screen w-full flex justify-center items-center text-center flex-col fadeOut bg-black">
         <p className="text-7xl text-white">Launching Our Spaceship...</p>
         <div className="h-80 w-80 flex justify-center items-center">
           <Image src="/spaceship.png" height={200} width={200} alt="rocket" className="shake"/>
@@ -25,59 +25,74 @@ export default function Home() {
               <p className="text-white text-center lg:text-xl">Explore | Discover | Together</p>
             </div>
           </div>
+          
+          <div className="bg-blue-950">
 
-          {/* ABOUT US */}
-          <div className="flex flex-col justify-center items-center bg-blue-950 h-full">
-            <h1 className="text-4xl text-white mt-8 lg:text-5xl">Welcome to MYAA</h1>
-            <br />
-            <div className="flex flex-row">
-              <div className="flex flex-col justify-center items-center">
-                <Image
-                  src="/aboutus/space1.jpg"
-                  height={250}
-                  width={250}
-                  alt="space 1"
-                  className="rounded-full mb-4"
-                />
-                <h2 className="text-white text-3xl font-semibold mb-4 text-center">Our Story</h2>
-                <p className="text-white text-center w-10/12 mb-8">
-                  Behind MYAA is a team of passionate high school students with a strong interest in space and backgrounds in STEM. MYAA is dedicated to connecting like-minded individuals interested in space science and technology.
-                </p>
+            {/* ABOUT US */}
+
+            <div className="flex flex-col justify-center items-center h-full">
+              <h1 className="text-4xl text-white mt-8 lg:text-5xl">Welcome to MYAA</h1>
+              <br />
+              <div className="flex flex-col lg:flex-row">
+                <div className="flex flex-col justify-center items-center">
+                  <Image
+                    src="/aboutus/space1.jpg"
+                    height={250}
+                    width={250}
+                    alt="space 1"
+                    className="rounded-full mb-4"
+                  />
+                  <h2 className="text-white text-3xl font-semibold mb-4 text-center">Our Story</h2>
+                  <p className="text-white text-center w-10/12 mb-8">
+                    Behind MYAA is a team of passionate high school students with a strong interest in space and backgrounds in STEM. MYAA is dedicated to connecting like-minded individuals interested in space science and technology.
+                  </p>
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                  <Image
+                    src="/aboutus/space2.jpg"
+                    height={250}
+                    width={250}
+                    alt="space 2"
+                    className="rounded-full mb-4"
+                  />
+                  <h2 className="text-white text-3xl font-semibold mb-4 text-center">Our Community</h2>
+                  <p className="text-white text-center w-10/12 mb-8">
+                    The association is a friendly community open to all youth in Mississauga, with exclusive guest speakers, workshops/seminars, volunteering/networking opportunities, and fun events!
+                  </p>
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                  <Image
+                    src="/aboutus/space3.jpg"
+                    height={250}
+                    width={250}
+                    alt="space 3"
+                    className="rounded-full mb-4"
+                  />
+                  <h2 className="text-white text-3xl font-semibold mb-4 text-center">Our Mission</h2>
+                  <p className="text-white text-center w-10/12 mb-8">
+                    With a mission to inspire curiosity, exploration, and innovation, MYAA is a dynamic non-profit organization dedicated to fostering and fueling a passion for astrophysics and astronautical technology amongst youth.
+                  </p>
+                </div>
               </div>
-              <div className="flex flex-col justify-center items-center">
-                <Image
-                  src="/aboutus/space2.jpg"
-                  height={250}
-                  width={250}
-                  alt="space 2"
-                  className="rounded-full mb-4"
-                />
-                <h2 className="text-white text-3xl font-semibold mb-4 text-center">Our Community</h2>
-                <p className="text-white text-center w-10/12 mb-8">
-                  The association is a friendly community open to all youth in Mississauga, with exclusive guest speakers, workshops/seminars, volunteering/networking opportunities, and fun events!
-                </p>
-              </div>
-              <div className="flex flex-col justify-center items-center">
-                <Image
-                  src="/aboutus/space3.jpg"
-                  height={250}
-                  width={250}
-                  alt="space 3"
-                  className="rounded-full mb-4"
-                />
-                <h2 className="text-white text-3xl font-semibold mb-4 text-center">Our Mission</h2>
-                <p className="text-white text-center w-10/12 mb-8">
-                  With a mission to inspire curiosity, exploration, and innovation, MYAA is a dynamic non-profit organization dedicated to fostering and fueling a passion for astrophysics and astronautical technology amongst youth.
-                </p>
+            </div>
+
+            <div className="mt-8">
+                <h1 className="text-5xl font-medium text-center text-white">Upcoming Events</h1>
+                <Event date="TBD" heading="First Meeting" body="We invite you to explore the depths of the universe with us at our very first  in-person meeting. Stay tuned for guest speakers, workshops, and volunteer opportunities!" subtitle="Spring 2024"/>
+                <div className="flex justify-center items-center">
+                  <div className="py-8 bg-blue-950 w-10/12">
+                    <hr className='hr-1'/>
+                  </div>
               </div>
             </div>
           </div>
+          
+          {/* CONTACT */}
 
-          {/* EVENTS */}
-          <div>
-            <h1>Upcoming Events</h1>
-            <Event />
+          <div className='bg-black'>
+            <p>ajsdoasadsi</p>
           </div>
+          
         </div>
       </div>
     </main>
--- components/Event.tsx
+++ components/Event.tsx
@@ -1,9 +1,75 @@
-import React from 'react'
+'use client'
 
-const Event = () => {
-  return (
-    <div>Event</div>
-  )
+import React, { useState, useEffect, FC } from 'react';
+
+interface PageProps {
+    date: string
+    heading: string
+    body: string
+    subtitle: string
 }
 
-export default Event
+const Event: FC<PageProps> = ({ date, heading, body, subtitle }) => {
+    const [isMobile, setIsMobile] = useState(false);
+    const [isExpanded, setIsExpanded] = useState(false);
+    const maxLength = 100;
+    const displayBody = isExpanded ? body : body.substring(0, maxLength) + '...';
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 767);
+        };
+
+        handleResize(); 
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const handleToggle = () => {
+        setIsExpanded(!isExpanded);
+    };
+
+    return (
+        <main className='my-8'>
+            <div className='flex w-full justify-center items-center'>
+                <div className='w-10/12 flex flex-col lg:flex-row justify-evenly items-start'>
+                    {isMobile ? (
+                        <div>
+                            <h1 className='text-3xl text-white font-medium mb-4'>Date: {date}</h1>
+                            <h1 className='text-3xl text-white mb-4'>{heading}</h1>
+                            <p className='text-gray-400 text-md mb-4'>{subtitle}</p>
+                            <p className='text-md text-gray-400'>{displayBody}</p>
+                            {body.length > maxLength && (
+                                <button onClick={handleToggle}>
+                                    {isExpanded ? 'Show Less' : 'Read More'}
+                                </button>
+                            )}
+                        </div>
+                    ) : (
+                        <div className='flex justify-evenly flex-row w-full'>
+                            <h1 className='text-3xl text-white font-medium mb-4 w-1/3'>Date: {date}</h1>
+                            <div className='flex flex-col w-1/3'>
+                                <h1 className='text-3xl text-white mb-4'>{heading}</h1>
+                                <p className='text-md text-gray-400'>{displayBody}</p>
+                                {body.length > maxLength && (
+                                    <button onClick={handleToggle}>
+                                        <p className='text-gray-400 text-left'>{isExpanded ? 'Show Less' : 'Read More'}</p>
+                                    </button>
+                                )}
+                            </div>
+                            <div className='flex flex-col w-1/3'>
+                                <p className='text-gray-400 text-md mb-4 text-right'>{subtitle}</p>
+                            </div>
+                        </div>
+                    )}
+                    
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default Event;
