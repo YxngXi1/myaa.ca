a5bb7d5a6e62bbc52930801677dee0709cf97411
YxngXi1 <xuetianyang@gmail.com>
2024-08-06 22:26:31 -0400

    done blogs

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import React, { FC } from 'react';
+import blogs from '@/utils/blogData';
+import { useParams } from 'next/navigation';
+import Link from 'next/link';
+import Image from 'next/image'
+
+const Page: FC = () => {
+  const params = useParams();
+  const { slug } = params;
+
+  const blog = blogs.find((blog) => blog.slug === slug);
+
+  if (!blog) {
+    return <p>Blog not found</p>;
+  }
+
+  return (
+    <main>
+      <div className='mx-12 flex flex-col gap-y-4 mt-4'>
+        <h1 className='text-6xl w-[750px]'>{blog.title}</h1>
+        <Image
+          src={blog.cover}
+          height={750}
+          width={750}
+          alt='blog cover'
+           />
+        <p className='text-xl'>By: <span className='text-blue-600'>{blog.author}</span></p>
+        <p className='text-xl'>{blog.date}</p>
+        
+        <p>{blog.para1}</p>
+        <p>{blog.para2}</p>
+        <Image
+          src={blog.image1}
+          height={400}
+          width={400}
+          alt='image 1'
+           />
+        <p>{blog.para3}</p>
+        <p>{blog.para4}</p>
+        <Image
+          src={blog.image2}
+          height={400}
+          width={400}
+          alt='image 1'
+           />
+        <p>{blog.para5}</p>
+          <Image
+        src={blog.image3}
+        height={400}
+        width={400}
+        alt='image 1'
+          />
+      </div>
+      <div className='flex w-full justify-center items-center mb-4'>
+        <Link href='/'>
+          <button className='p-4 bg-white text-black border-black border hover:bg-gray-300 rounded-full'>&larr; Return To Home</button>
+        </Link>
+      </div>
+    </main>
+  );
+};
+
+export default Page;
--- components/Blogs.tsx
+++ components/Blogs.tsx
@@ -2,45 +2,15 @@ import React from 'react';
 import { FC } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-
-interface BlogDetails {
-    slug: string;
-    cover: string;
-    title: string;
-    date: string;
-    author: string;
-}
-
-const blogs: BlogDetails[] = [
-  {
-    slug: 'dark-energy',
-    cover: '/space4.jpg',
-    title: 'Dark Energy - The Greatest Mystery in the Universe',
-    date: 'June 22, 2024',
-    author: 'Nivedya Sudhir'
-  },
-  {
-    slug: 'dark-energy',
-    cover: '/space4.jpg',
-    title: 'Dark Energy - The Greatest Mystery in the Universe',
-    date: 'June 22, 2024',
-    author: 'Nivedya Sudhir'
-  },
-  {
-    slug: 'dark-energy',
-    cover: '/space4.jpg',
-    title: 'Dark Energy - The Greatest Mystery in the Universe',
-    date: 'June 22, 2024',
-    author: 'Nivedya Sudhir'
-  },
-];
+import blogs from '@/utils/blogData'
 
 const Page: FC = () => {
+
   return (
     <main className='flex justify-center items-center w-full '>
       <div className='grid grid-cols-2 gap-x-32 gap-y-16 '>
         {blogs.map((blog, index) => (
-          <Link href={blog.slug} key={index}>
+          <Link href={`/blog/${blog.slug}`} key={index}>
             <div className=" w-[320px] rounded-md shadow-[0_0_6px_2px_rgba(255,255,255,0.5)] hover:bg-[rgba(0,0,0,0.75)] transition duration-500 hover:cursor-pointer ">
               <div className='flex flex-col justify-cener items-center text-left mx-8'>
                 <div className="relative w-[220px] h-[220px] overflow-hidden rounded-md mb-8 mt-8">
